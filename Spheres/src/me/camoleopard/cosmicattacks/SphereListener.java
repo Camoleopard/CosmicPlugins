44a328a329c56d2103ec9304128767244e16d207
Kobey Rafferty <Camoleopard@>
2015-08-24 17:56:53 +0800

    Removed old comments

--- Spheres/src/me/camoleopard/cosmicattacks/SphereListener.java
+++ Spheres/src/me/camoleopard/cosmicattacks/SphereListener.java
@@ -26,19 +26,7 @@ public class SphereListener implements Listener {
 	
 	public static List<Player> players = new ArrayList<Player>();
 	
-	/*public static List<Player> getPlayersInRadius(Player p, int radius){
-			
-			double maxDist = radius;
-			for (Player other : Bukkit.getOnlinePlayers()) {
-			  if (other.getLocation().distance(p.getLocation()) <= maxDist) {
-				players.add(other); 
-				 
-			  }
-			  
-			  
-			}
-			return players;
-		}*/
+	
 
 	
 	
